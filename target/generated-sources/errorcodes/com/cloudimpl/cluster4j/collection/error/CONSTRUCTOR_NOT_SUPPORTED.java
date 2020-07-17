e01fffde60997042ba68be52a323b63534ffc0ab
nuwansa <nuwan077@gmail.com>
2020-07-17 22:55:32 +0530

    remove generated code

--- target/generated-sources/errorcodes/com/cloudimpl/cluster4j/collection/error/CONSTRUCTOR_NOT_SUPPORTED.java
+++
@@ -1,9 +0,0 @@
-package com.cloudimpl.cluster4j.collection.error;
-
-import com.cloudimpl.error.core.ErrorBuilder;
-
-public class CONSTRUCTOR_NOT_SUPPORTED extends ErrorBuilder {
-  CONSTRUCTOR_NOT_SUPPORTED() {
-     withCode(com.cloudimpl.cluster4j.collection.error.Collection.CONSTRUCTOR_NOT_SUPPORTED);
-  }
-}
--- target/generated-sources/errorcodes/com/cloudimpl/cluster4j/collection/error/KEY_VIOLATION.java
+++
@@ -1,9 +0,0 @@
-package com.cloudimpl.cluster4j.collection.error;
-
-import com.cloudimpl.error.core.ErrorBuilder;
-
-public class KEY_VIOLATION extends ErrorBuilder {
-  KEY_VIOLATION() {
-     withCode(com.cloudimpl.cluster4j.collection.error.Collection.KEY_VIOLATION);
-  }
-}
